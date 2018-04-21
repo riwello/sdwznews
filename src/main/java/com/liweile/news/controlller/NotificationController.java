04258d434654cca13ace584a48f76b4a949bcff5
liweile <464427219@qq.com>
2018-04-21 13:56:50 +0800

    websocket 发送json格式

--- src/main/java/com/liweile/news/controlller/NotificationController.java
+++ src/main/java/com/liweile/news/controlller/NotificationController.java
@@ -5,12 +5,14 @@ import com.liweile.news.handler.CmdHandler;
 import com.liweile.news.model.Notification;
 import com.liweile.news.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -24,8 +26,13 @@ public class NotificationController {
 
     @RequestMapping("/add")
     public ResponseEntity addNotification(@RequestParam String username, @RequestParam String content) {
-        CmdHandler.sendMassage(content);
-        return notificationService.addNotification(new Notification(username, new Date(), content));
+        Notification notification = new Notification(username, new Date(), content);
+        HashMap<String , String> map = new HashMap<>();
+        map.put("username",username);
+        map.put("content",content);
+        JSONObject jsonObject = new JSONObject(map);
+        CmdHandler.sendMassage(jsonObject.toString());
+        return notificationService.addNotification(notification);
 
 
     }
